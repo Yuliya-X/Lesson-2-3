ac5b9f968ec63b70d28f635274303314874589eb
Yuliya <darkwader@mail.ru>
2023-02-18 20:21:48 +0300

    внесли 3й алгоритм (задача про день недели

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// воод строки 
+int promt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+// ограничение данных - будние дни
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+// ограничение данных "не день недели"
+bool ValidateWeekDay(int weekDay)
+{
+    if (weekDay > 0) 
+    if (weekDay <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+// воод данных пользователем 
+int weekDay = promt("Введите день недели: "); 
+// определитель дня недели
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Ура, выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Эх, ещё работать и работать...");
+    }
+}
