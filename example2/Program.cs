b8e9305f32a46142fb489b8126a6c35c428ffb25
Yuliya <darkwader@mail.ru>
2023-02-18 13:25:45 +0300

    example2+

---
+++ example2/Program.cs
@@ -0,0 +1,32 @@
+﻿int promt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10; 
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = promt("Введите число: ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirRank(number));
+}
